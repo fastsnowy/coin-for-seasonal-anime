5d858148a41f32f43e699b9c6b256ba95ce3a271
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-01-29 01:38:21 +0900

    🏷️ annictの型の定義

---
+++ src/types/annict.ts
@@ -0,0 +1,20 @@
+export type annictWorks = {
+  nodes: nodes[]
+}
+
+export type nodes = {
+  __typename: string
+  annictId: number
+  media: string
+  image: _images | undefined
+  officialSiteUrl: string | undefined
+  title: string
+  twitterUsername: string | undefined
+}
+
+type _images = {
+  __typename: string
+  recommendedImageUrl: string
+  facebookOgImageUrl: string
+  twitterAvatarUrl: string
+}
