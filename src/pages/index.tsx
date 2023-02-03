5ec34efffa2cd1598691033b2551162bafbb0ff9
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-02-04 02:10:03 +0900

    ♻️ トップページを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,24 @@
-import { GetStaticProps, NextPage } from 'next'
-import { AppShell, Container, SimpleGrid, Stack, Title } from '@mantine/core'
-import type { annictWorks } from '@/types/annict'
-import { GET_ANIME_DETAILS } from '@/gql'
-import { headers, ANNICT_URL } from '@/libs/annict'
-import { AnimeCard } from '@/components/AnimeCard'
-import { Layout } from '@/layouts'
-import { ReactElement } from 'react'
+import Link from 'next/link'
 
-type searchWorksProps = {
-  searchWorks: annictWorks
-}
+import { Button, Stack, Title } from '@mantine/core'
 
-export default function Home({ searchWorks }: searchWorksProps) {
+import { getSeasons } from '@/utils/getseason'
+
+export default function Season() {
+  const season = getSeasons()
   return (
     <>
       <Stack align='center' justify='center'>
         <Title order={1} className='underline text-red-300 p-3 px-2'>
           テスト表示
         </Title>
+        <Link href='/seasons/current'>
+          <Button>今期({season.current})</Button>
+        </Link>
+        <Link href='/seasons/next'>
+          <Button>来期({season.next})</Button>
+        </Link>
       </Stack>
-      <Container size='xl'>
-        <SimpleGrid
-          cols={3}
-          breakpoints={[
-            { maxWidth: 'sm', cols: 2 },
-            { maxWidth: 'xs', cols: 1 },
-          ]}
-        >
-          {searchWorks.nodes.map((work) => (
-            <div key={work.annictId}>
-              <AnimeCard work={work} />
-            </div>
-          ))}
-        </SimpleGrid>
-      </Container>
     </>
   )
-}
-
-Home.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch(ANNICT_URL, {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(GET_ANIME_DETAILS),
-  })
-  const data = await response.json()
-  console.log(data.data.searchWorks)
-  return {
-    props: { searchWorks: data.data.searchWorks },
-    revalidate: 60,
-  }
 }
