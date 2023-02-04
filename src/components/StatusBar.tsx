1e3d4cbaf0b32d733da28f7e0091d6919a2be6f0
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-02-04 18:54:45 +0900

    ✨ 桁区切りで表示

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,17 +1,18 @@
-import { useRecoilValue } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 
 import { Button, Flex, Text } from '@mantine/core'
 
+import { AtomIsCurrentModalOpened } from '@/global/atoms'
 import { selectorTotalCoinCurrentSeason, selectorTotalCoinNextSeason } from '@/global/selectors'
 
 export function CurrentStatus() {
   const totalValue = useRecoilValue(selectorTotalCoinCurrentSeason)
-
+  const setModalOpened = useSetRecoilState(AtomIsCurrentModalOpened)
   return (
     <Flex align='center' gap='lg' justify='center'>
       <Text>賭けたコイン</Text>
-      <Text>🪙{totalValue}枚</Text>
-      <Button variant='light' color='cyan'>
+      <Text>🪙{totalValue.toLocaleString()}枚</Text>
+      <Button variant='light' color='cyan' onClick={() => setModalOpened(true)}>
         <Text>結果を表示</Text>
       </Button>
     </Flex>
@@ -20,12 +21,12 @@ export function CurrentStatus() {
 
 export function NextStatus() {
   const totalValue = useRecoilValue(selectorTotalCoinNextSeason)
-
+  const setModalOpened = useSetRecoilState(AtomIsCurrentModalOpened)
   return (
     <Flex align='center' gap='lg' justify='center'>
       <Text>賭けたコイン</Text>
-      <Text>🪙{totalValue}枚</Text>
-      <Button variant='light' color='cyan'>
+      <Text>🪙{totalValue.toLocaleString()}枚</Text>
+      <Button variant='light' color='cyan' onClick={() => setModalOpened(true)}>
         <Text>結果を表示</Text>
       </Button>
     </Flex>
