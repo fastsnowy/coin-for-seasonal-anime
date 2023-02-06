d6095b46b9898d65ffe66046a6f906bf83096f3d
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-02-06 21:17:37 +0900

    💄 トップページの調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,71 @@
 import Link from 'next/link'
+import { ReactElement } from 'react'
+import { TiLightbulb, TiWarningOutline } from 'react-icons/ti'
 
-import { Button, Stack, Title } from '@mantine/core'
+import { Alert, Button, Container, Group, SimpleGrid, Text } from '@mantine/core'
 
+import { Layout } from '@/layouts'
 import { getSeasons } from '@/utils/getseason'
 
-export default function Season() {
+export default function Home() {
   const season = getSeasons()
   return (
     <>
-      <Stack align='center' justify='center'>
-        <Title order={1} className='underline text-red-300 p-3 px-2'>
-          テスト表示
-        </Title>
-        <Link href='/seasons/current'>
-          <Button>今期({season.current})</Button>
-        </Link>
-        <Link href='/seasons/next'>
-          <Button>来期({season.next})</Button>
-        </Link>
-      </Stack>
+      <Container p='md' size='md'>
+        <SimpleGrid cols={1} spacing='xs'>
+          <Text align='center' size='xl'>
+            アニメに対する期待度を「アニメコイン」を賭けて表そう！
+          </Text>
+          <Group position='center' p='md'>
+            <Link href='/seasons/current'>
+              <Button variant='outline'>
+                今期(
+                {season.current
+                  .replace('winter', '冬')
+                  .replace('spring', '春')
+                  .replace('summer', '夏')
+                  .replace('autumn', '秋')}
+                )に賭ける
+              </Button>
+            </Link>
+            <Link href='/seasons/next'>
+              <Button variant='outline'>
+                来期(
+                {season.next
+                  .replace('winter', '冬')
+                  .replace('spring', '春')
+                  .replace('summer', '夏')
+                  .replace('autumn', '秋')}
+                )に賭ける
+              </Button>
+            </Link>
+          </Group>
+
+          <Alert title='about' icon={<TiLightbulb />}>
+            <Text>
+              このサイトは
+              <Text
+                component='a'
+                target='_blank'
+                href='https://developers.annict.com'
+                className='underline'
+              >
+                Annict GraphQL API
+              </Text>
+              を用いてアニメの情報を取得しています。 作った人→@pixy_slp
+            </Text>
+          </Alert>
+          <Alert title='注意' color='red' icon={<TiWarningOutline />} className='grow-0'>
+            <Text>
+              このサイトはネタサイトです。「アニメコイン」は実際の金融通貨等と一切関係ありません。
+            </Text>
+          </Alert>
+        </SimpleGrid>
+      </Container>
     </>
   )
+}
+
+Home.getLayout = function getLayout(pages: ReactElement) {
+  return <Layout>{pages}</Layout>
 }
