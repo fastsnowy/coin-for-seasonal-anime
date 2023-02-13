7529620186c4b36068adbb670bf23f5380a43934
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-02-13 15:23:11 +0900

    🩺 環境変数から取得

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,2 +1,2 @@
-export const DEPLOY_URL = process.env.DEPLOY_URL
-export const GITHUB_URL = 'https://github.com/fastsnowy/anime-coin'
+export const DEPLOY_URL = process.env.NEXT_PUBLIC_DEPLOY_URL
+export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL
