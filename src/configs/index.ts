5746da7a5e1f06b4a28436ccb0cfd784454795cb
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2023-04-20 17:43:48 +0900

    ✨ supabaseへの接続

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,5 @@
 export const DEPLOY_URL = process.env.NEXT_PUBLIC_DEPLOY_URL
 export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL
 export const TWITTER_URL = process.env.NEXT_PUBLIC_TWITTER_URL
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
