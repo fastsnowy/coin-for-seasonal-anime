a55c42d547796e4d36905e82177d764f2105626c
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-01-03 13:54:21 +0900

    ⚡️ Update og:site_name meta tag in BaseHead component

--- src/components/BaseHead.tsx
+++ src/components/BaseHead.tsx
@@ -9,7 +9,7 @@ export function SEO({ title, currentUrl }: { title: string; currentUrl: string }
       <meta name='twitter:card' content='summary_large_image' />
       <meta property='og:title' content={`${title} | coin for seasonal anime`} />
       <meta property='og:url' content={`https://${DEPLOY_URL}/${currentUrl}`} />
-      <meta property='og:site_name' content={`${title} | coin for seasonal anime`} />
+      <meta property='og:site_name' content={`coin for seasonal anime`} />
       <meta property='og:description' content='アニメに対する期待度を「コイン」を賭けて表そう' />
     </Head>
   )
