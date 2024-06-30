de1607e5a6ffbf9f7d749fa4f14255b9c5970ea9
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-06-30 12:10:31 +0900

    🚧 ✨ /, /seasonsをapp directoryに移動

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>アニメに対する期待度を「コイン」を賭けて表そう！</h1>
+    </div>
+  );
+}
---
+++ src/app/seasons/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>各シーズン一覧</h1>
+    </div>
+  );
+}
