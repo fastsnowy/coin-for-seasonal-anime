7ace68fd5a375207eae19c6e2812b11bca383a01
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2025-03-23 19:45:15 +0900

    🔥 Remove unused archives

--- archives/src/libs/annict.ts
+++
@@ -1,6 +0,0 @@
-export const headers = {
-  "content-type": "application/json",
-  Authorization: `Bearer ${process.env.ANNICT_TOKEN}`,
-};
-
-export const ANNICT_URL = "https://api.annict.com/graphql";
--- archives/src/types/coins.ts
+++
@@ -1,5 +0,0 @@
-export type betAnimes = {
-  annict_id: number;
-  coin_value: number;
-  season: string;
-}[];
