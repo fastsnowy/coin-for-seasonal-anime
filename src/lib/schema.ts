51d6e9121aebd0beafe88cd6409d02b459abc380
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2025-03-24 00:49:10 +0900

    ✨ Add Supabase schema generation configuration and types

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,176 +1,178 @@
+
 export type Json =
   | string
   | number
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   graphql_public: {
     Tables: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       graphql: {
         Args: {
-          operationName?: string;
-          query?: string;
-          variables?: Json;
-          extensions?: Json;
-        };
-        Returns: Json;
-      };
-    };
+          operationName?: string
+          query?: string
+          variables?: Json
+          extensions?: Json
+        }
+        Returns: Json
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
   public: {
     Tables: {
       bet_coins: {
         Row: {
-          annict_id: number | null;
-          coin_value: number | null;
-          created_at: string | null;
-          created_id: string | null;
-          id: string;
-          season: string | null;
-        };
+          annict_id: number | null
+          coin_value: number | null
+          created_at: string | null
+          created_id: string | null
+          id: string
+          season: string | null
+        }
         Insert: {
-          annict_id?: number | null;
-          coin_value?: number | null;
-          created_at?: string | null;
-          created_id?: string | null;
-          id?: string;
-          season?: string | null;
-        };
+          annict_id?: number | null
+          coin_value?: number | null
+          created_at?: string | null
+          created_id?: string | null
+          id?: string
+          season?: string | null
+        }
         Update: {
-          annict_id?: number | null;
-          coin_value?: number | null;
-          created_at?: string | null;
-          created_id?: string | null;
-          id?: string;
-          season?: string | null;
-        };
-        Relationships: [];
-      };
+          annict_id?: number | null
+          coin_value?: number | null
+          created_at?: string | null
+          created_id?: string | null
+          id?: string
+          season?: string | null
+        }
+        Relationships: []
+      }
       dev_coins: {
         Row: {
-          annict_id: number | null;
-          coin_value: number | null;
-          created_at: string | null;
-          created_id: string | null;
-          delete_id: string | null;
-          id: string;
-          season: string | null;
-        };
+          annict_id: number | null
+          coin_value: number | null
+          created_at: string | null
+          created_id: string | null
+          delete_id: string | null
+          id: string
+          season: string | null
+        }
         Insert: {
-          annict_id?: number | null;
-          coin_value?: number | null;
-          created_at?: string | null;
-          created_id?: string | null;
-          delete_id?: string | null;
-          id?: string;
-          season?: string | null;
-        };
+          annict_id?: number | null
+          coin_value?: number | null
+          created_at?: string | null
+          created_id?: string | null
+          delete_id?: string | null
+          id?: string
+          season?: string | null
+        }
         Update: {
-          annict_id?: number | null;
-          coin_value?: number | null;
-          created_at?: string | null;
-          created_id?: string | null;
-          delete_id?: string | null;
-          id?: string;
-          season?: string | null;
-        };
-        Relationships: [];
-      };
+          annict_id?: number | null
+          coin_value?: number | null
+          created_at?: string | null
+          created_id?: string | null
+          delete_id?: string | null
+          id?: string
+          season?: string | null
+        }
+        Relationships: []
+      }
       howamun: {
         Row: {
-          count: number | null;
-          created_at: string;
-          id: number;
-        };
+          count: number | null
+          created_at: string
+          id: number
+        }
         Insert: {
-          count?: number | null;
-          created_at?: string;
-          id?: number;
-        };
+          count?: number | null
+          created_at?: string
+          id?: number
+        }
         Update: {
-          count?: number | null;
-          created_at?: string;
-          id?: number;
-        };
-        Relationships: [];
-      };
+          count?: number | null
+          created_at?: string
+          id?: number
+        }
+        Relationships: []
+      }
       "howamun.day-likes": {
         Row: {
-          created_at: string;
-          id: number;
-        };
+          created_at: string
+          id: number
+        }
         Insert: {
-          created_at?: string;
-          id?: number;
-        };
+          created_at?: string
+          id?: number
+        }
         Update: {
-          created_at?: string;
-          id?: number;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string
+          id?: number
+        }
+        Relationships: []
+      }
+    }
     Views: {
       annict_coin_value_total: {
         Row: {
-          annict_id: number | null;
-          total_coin_value: number | null;
-        };
-        Relationships: [];
-      };
+          annict_id: number | null
+          total_coin_value: number | null
+        }
+        Relationships: []
+      }
       dev_coin_value_view: {
         Row: {
-          annict_id: number | null;
-          season: string | null;
-          total_coin_value: number | null;
-        };
-        Relationships: [];
-      };
+          annict_id: number | null
+          season: string | null
+          total_coin_value: number | null
+        }
+        Relationships: []
+      }
       test_coin: {
         Row: {
-          annict_id: number | null;
-          season: string | null;
-          total_coin_value: number | null;
-        };
-        Relationships: [];
-      };
+          annict_id: number | null
+          season: string | null
+          total_coin_value: number | null
+        }
+        Relationships: []
+      }
       total_coin_value_view: {
         Row: {
-          annict_id: number | null;
-          season: string | null;
-          total_coin_value: number | null;
-        };
-        Relationships: [];
-      };
-    };
+          annict_id: number | null
+          season: string | null
+          total_coin_value: number | null
+          uu: number | null
+        }
+        Relationships: []
+      }
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type PublicSchema = Database[Extract<keyof Database, "public">];
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -183,7 +185,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -191,11 +193,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R;
+        Row: infer R
       }
       ? R
       : never
-    : never;
+    : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -206,17 +208,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I;
+        Insert: infer I
       }
       ? I
       : never
-    : never;
+    : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -227,17 +229,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U;
+        Update: infer U
       }
       ? U
       : never
-    : never;
+    : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -250,14 +252,14 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never;
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof Database
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -265,4 +267,4 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never;
+    : never
